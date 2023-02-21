b4000abf888c099d93e0a2837835fad901882c74
gsofter <real.kingstar@outlook.com>
2023-02-21 12:54:35 -0600

    update stache

--- src/utils/__tests__/stache.test.ts
+++ src/utils/__tests__/stache.test.ts
@@ -1,10 +1,11 @@
 import * as anchor from "@project-serum/anchor";
+import { Keypair } from "@solana/web3.js";
 import { createStache } from "../stache";
 import { airdropSol } from "../utils";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({
-  path: '.env.test',
+  path: '.env',
 });
 
 
@@ -18,24 +19,40 @@ function randomName() {
 
 // for setting up the keychaink
 const domain = randomName();
-const treasury = anchor.web3.Keypair.generate();
 const renameCost = new anchor.BN(anchor.web3.LAMPORTS_PER_SOL * 0.01);
 const username = randomName(); // used as the keychain + stache name
-// const provider = anchor.AnchorProvider.env();
-const admin = anchor.web3.Keypair.generate();
+const provider = anchor.AnchorProvider.env();
+const adminWallet = Keypair.fromSecretKey(
+  Buffer.from(
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    JSON.parse(require("fs").readFileSync(process.env.ADMIN_WALLET, {
+      encoding: 'utf-8'
+    }))
+  )
+)
+const treasury = Keypair.fromSecretKey(
+  Buffer.from(
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    JSON.parse(require("fs").readFileSync(process.env.TREASURY_WALLET, {
+      encoding: 'utf-8'
+    }))
+  )
+)
 
+console.log("adminWallet => ", adminWallet.publicKey.toBase58())
+console.log("provider.wallet.publicKey => ", provider.wallet.publicKey.toBase58())
+console.log("treasurey.publicKey => ", treasury.publicKey.toBase58())
 
 describe('stache', () => {
 
   beforeAll(async () => {
     // const connection = provider.connection
-    // await airdropSol(connection, provider.wallet.publicKey, 50)
-    // await airdropSol(connection, admin.publicKey, 50)
-
-  })
+    // await airdropSol(connection, provider.wallet.publicKey, 1)
+    // await airdropSol(connection, adminWallet.publicKey, 1)
+  }, 10000)
 
   it('createStache', async () => {
-    // await createStache(provider, domain, username, admin)
+    await createStache(provider, domain, username)
     expect(1).toBe(1)
   }, 10000);
 });
--- src/utils/stache.ts
+++ src/utils/stache.ts
@@ -1,29 +1,22 @@
-import * as anchor from '@project-serum/anchor';
-import { Idl, Program, web3 } from '@project-serum/anchor';
+import * as anchor from "@project-serum/anchor";
+import { Idl, Program, web3 } from "@project-serum/anchor";
 
-import { execSync } from 'child_process';
+import { execSync } from "child_process";
 
-import kcidl from '../idls/keychain.json';
-import stidl from '../idls/stashe.json';
+import kcidl from "../idls/keychain.json";
+import stidl from '../idls/stashe.json'
 
+import { Keypair, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js";
 import {
-    Keypair,
-    PublicKey,
-    sendAndConfirmTransaction,
-    SystemProgram,
-    Transaction,
-} from '@solana/web3.js';
-import {
-    createNFTMint,
-    createTokenMint,
+    createNFTMint, createTokenMint,
     findStachePda,
     findDomainPda,
     findDomainStatePda,
     findKeychainKeyPda,
     findKeychainPda,
-    findKeychainStatePda,
-} from './utils';
-import * as assert from 'assert';
+    findKeychainStatePda
+} from "./utils";
+import * as assert from "assert";
 import {
     ASSOCIATED_TOKEN_PROGRAM_ID,
     createAssociatedTokenAccount,
@@ -31,169 +24,137 @@ import {
     createMint,
     createMintToCheckedInstruction,
     createTransferCheckedInstruction,
-    createTransferCheckedWithFeeInstruction,
-    createTransferInstruction,
+    createTransferCheckedWithFeeInstruction, createTransferInstruction,
     getAssociatedTokenAddress,
     getAssociatedTokenAddressSync,
-    mintToChecked,
-    TOKEN_PROGRAM_ID,
-    transferChecked,
-} from '@solana/spl-token';
+    mintToChecked, TOKEN_PROGRAM_ID,
+    transferChecked
+} from "@solana/spl-token";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config({
+    path: '.env',
+});
+
 
 const KeychainIdl = kcidl as Idl;
 const StacheIdl = stidl as Idl;
 
-const KeychainProgId: PublicKey = new PublicKey(KeychainIdl.metadata.address);
+const renameCost = new anchor.BN(anchor.web3.LAMPORTS_PER_SOL * 0.01);
 
-///// this test is set up to run against a local validator with the assumptions:
-///// 1. the keychain program is deployed to the local validator at the address in the keychain idl
-///// 2. the key set up in anchor.toml is funded with SOL (to deploy stache)
-
-// then u can run: anchor test --provider.cluster localnet --skip-local-validator
-
-const deployKeychain = () => {
-    const deployCmd = `solana program deploy --url localhost -v --program-id $(pwd)/../keychain/target/deploy/keychain-keypair.json $(pwd)/../keychain/target/deploy/keychain.so`;
-    execSync(deployCmd);
-};
-
-
+const admin = Keypair.fromSecretKey(
+    Buffer.from(
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        JSON.parse(require("fs").readFileSync(process.env.ADMIN_WALLET, {
+            encoding: 'utf-8'
+        }))
+    )
+)
+const treasury = Keypair.fromSecretKey(
+    Buffer.from(
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        JSON.parse(require("fs").readFileSync(process.env.TREASURY_WALLET, {
+            encoding: 'utf-8'
+        }))
+    )
+)
 
 /**
  * Create stache
- *
- * @param provider
- * @param domain
- * @param username
- * @param admin
+ * 
+ * @param provider 
+ * @param domain 
+ * @param username 
  */
 export const createStache = async (
     provider: anchor.AnchorProvider,
     domain: string,
     username: string,
-    admin: anchor.web3.Keypair,
 ) => {
-    const connection = provider.connection;
 
-   
+    const stacheProgram = new Program(StacheIdl, StacheIdl.metadata.address, provider);
+    const keychainProgram = new Program(KeychainIdl, KeychainIdl.metadata.address, provider);
 
-    const stacheProgram = new Program(
-        StacheIdl,
-        StacheIdl.metadata.address,
-        provider,
-    );
-    const keychainProgram = new Program(
-        KeychainIdl,
-        KeychainIdl.metadata.address,
-        provider,
-    );
+    console.log("keychainProgram.programId =>", keychainProgram.programId)
 
     const [domainPda] = findDomainPda(domain, keychainProgram.programId);
-    const [userKeychainPda] = findKeychainPda(
-        username,
-        domain,
-        keychainProgram.programId,
-    );
 
-    const [stachePda, stachePdaBump] = findStachePda(
-        username,
-        domainPda,
-        stacheProgram.programId,
-    );
+    console.log("domainPda =>", domainPda.toBase58())
+    const [domainStatePda, domainStatePdaBump] = findDomainStatePda(domain, keychainProgram.programId);
 
-    const txid = await stacheProgram.methods
-        .createStache()
-        .accounts({
-            stache: stachePda,
-            keychain: userKeychainPda,
-            keychainProgram: keychainProgram.programId,
-            systemProgram: SystemProgram.programId,
-        })
-        .rpc();
+    const [userKeychainPda] = findKeychainPda(username, domain, keychainProgram.programId);
+    const [userKeychainStatePda] = findKeychainStatePda(userKeychainPda, domain, keychainProgram.programId);
+    const [userKeychainKeyPda] = findKeychainKeyPda(provider.wallet.publicKey, domain, keychainProgram.programId);
+
+
+    const [stachePda, stachePdaBump] = findStachePda(username, domainPda, stacheProgram.programId);
+
+    console.log(`creating keychain domain: ${domain}...`);
+
+    // first create the domain
+    let txid = await keychainProgram.methods.createDomain(domain, renameCost).accounts({
+        domain: domainPda,
+        domainState: domainStatePda,
+        authority: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+        treasury: treasury.publicKey
+    }).rpc();
+    console.log(`created keychain domain tx: ${txid}`);
+
+    txid = await stacheProgram.methods.createStache().accounts({
+        stache: stachePda,
+        keychain: userKeychainPda,
+        keychainProgram: keychainProgram.programId,
+        systemProgram: SystemProgram.programId,
+    }).rpc();
 
     const stache = await stacheProgram.account.currentStache.fetch(stachePda);
-};
+}
+
 
 /**
- *
+ * 
  * @param provider AnchorProvider
  * @param mint Minted Token Account
- * @param userAta
+ * @param userAta 
  * @param domain
  * @param username
  */
-export const stach = async (
-    provider: anchor.AnchorProvider,
-    mint: Keypair,
-    userAta: PublicKey,
-    domain: string,
-    username: string,
-) => {
+export const stach = async (provider: anchor.AnchorProvider, mint: Keypair, userAta: PublicKey, domain: string, username: string) => {
     const connection = provider.connection;
-    const stacheProgram = new Program(
-        StacheIdl,
-        StacheIdl.metadata.address,
-        provider,
-    );
-    const keychainProgram = new Program(
-        KeychainIdl,
-        KeychainIdl.metadata.address,
-        provider,
-    );
+    const stacheProgram = new Program(StacheIdl, StacheIdl.metadata.address, provider);
+    const keychainProgram = new Program(KeychainIdl, KeychainIdl.metadata.address, provider);
 
     const [domainPda] = findDomainPda(domain, keychainProgram.programId);
-    const [stachePda, stachePdaBump] = findStachePda(
-        username,
-        domainPda,
-        stacheProgram.programId,
-    );
+    const [stachePda, stachePdaBump] = findStachePda(username, domainPda, stacheProgram.programId);
 
-    const stacheMintAta = getAssociatedTokenAddressSync(
-        mint.publicKey,
-        stachePda,
-        true,
-    );
+    const stacheMintAta = getAssociatedTokenAddressSync(mint.publicKey, stachePda, true);
 
     // stash: this tx will create the stache's mint ata and deposit some tokens in there
     let tx = new Transaction().add(
-        createAssociatedTokenAccountInstruction(
-            provider.wallet.publicKey,
-            stacheMintAta,
-            stachePda,
-            mint.publicKey,
-        ),
-        createTransferCheckedInstruction(
-            userAta,
-            mint.publicKey,
-            stacheMintAta,
-            provider.wallet.publicKey,
-            100 * 1e9,
-            9,
-        ),
+        createAssociatedTokenAccountInstruction(provider.wallet.publicKey, stacheMintAta, stachePda, mint.publicKey),
+        createTransferCheckedInstruction(userAta, mint.publicKey, stacheMintAta, provider.wallet.publicKey, 100 * 1e9, 9)
     );
     let txid = await provider.sendAndConfirm(tx);
 
-    console.log(
-        `created stache mint ata: ${stacheMintAta.toBase58()}, and deposited 100 tokens, txid: ${txid}`,
-    );
+    console.log(`created stache mint ata: ${stacheMintAta.toBase58()}, and deposited 100 tokens, txid: ${txid}`);
     let tokenAmount = await connection.getTokenAccountBalance(stacheMintAta);
     console.log(`>> stache mint ata balance: ${tokenAmount.value.uiAmount}`);
     tokenAmount = await connection.getTokenAccountBalance(userAta);
     console.log(`>> user ata balance: ${tokenAmount.value.uiAmount}`);
 
+
     // now let's stash via the stash instruction
-    tx = await stacheProgram.methods
-        .stash(new anchor.BN(500 * 1e9))
-        .accounts({
-            stache: stachePda,
-            stacheAta: stacheMintAta,
-            mint: mint.publicKey,
-            owner: provider.wallet.publicKey,
-            fromToken: userAta,
-            systemProgram: SystemProgram.programId,
-            tokenProgram: TOKEN_PROGRAM_ID,
-            associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
-        })
-        .transaction();
+    tx = await stacheProgram.methods.stash(new anchor.BN(500 * 1e9)).accounts({
+        stache: stachePda,
+        stacheAta: stacheMintAta,
+        mint: mint.publicKey,
+        owner: provider.wallet.publicKey,
+        fromToken: userAta,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+    }).transaction();
 
     txid = await provider.sendAndConfirm(tx);
 
@@ -202,4 +163,5 @@ export const stach = async (
     console.log(`new stache mint ata balance: ${tokenAmount.value.uiAmount}`);
     tokenAmount = await connection.getTokenAccountBalance(userAta);
     console.log(`new user ata balance: ${tokenAmount.value.uiAmount}`);
-};
+}
+
